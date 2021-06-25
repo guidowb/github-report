e64086e6c5802d23db59e45ae8aedf8aa55bde31
Guido Westenberg <gwestenberg@pivotal.io>
2021-06-25 08:55:01 -0500

    Removed duplicate code; filter out named bots

--- report.py
+++ report.py
@@ -11,7 +11,7 @@ cache_path = '~/.cache'
 oauth_file = '~/.secrets/github-reports'
 oauth_token = ''
 
-verbose = True
+verbose = False
 
 timeframes = [
     {
@@ -66,189 +66,17 @@ our_orgs = [
     'vmware-tanzu'
 ]
 
-def to_date(source):
-    if source is None:
-        return None
-    if isinstance(source, str):
-        if re.match('[1-2]?[0-9]/[1-3]?[0-9]/[0-9]+', source) is not None:
-            return datetime.datetime.strptime(source, "%m/%d/%Y")
-        if re.match('[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+Z', source) is not None:
-            return datetime.datetime.strptime(source, '%Y-%m-%dT%H:%M:%SZ')
-        print('------- date? ', source)
-        return None
-    return source
-
-def in_range(date, daterange):
-    if daterange is None:
-        return True
-    date = to_date(date)
-    start = daterange.get('start')
-    if start is not None and date < start:
-        return False
-    end = daterange.get('end')
-    if end is not None and date > end:
-        return False
-    return True
-
-def overlaps_range(left, right, daterange):
-    if daterange is None:
-        return True
-    if left is not None:
-        left = to_date(left)
-        end = daterange.get('end')
-        if end is not None and left > end:
-            return False
-    if right is not None:
-        right = to_date(right)
-        start = daterange.get('start')
-        if start is not None and right < start:
-            return False
-    return True
-
-def range(start, end):
-    result = {}
-    if start is not None:
-        result['start'] = to_date(start)
-    if end is not None:
-        result['end'] = to_date(end)
-    return result
-
-def read_token():
-    global oauth_token
-    with open(os.path.expanduser(oauth_file)) as f:
-        oauth_token = f.read().replace('\n', '')
-
-def get_links(response):
-    link_header = response.headers.get('Link')
-    if link_header is None:
-        return {}
-    link_values = link_header.split(', ')
-    links = {}
-    for link in link_values:
-        parts = link.split('; rel=')
-        url = parts[0].strip('<>')
-        rel = parts[1].strip('"')
-        links[rel] = url
-    return links
-
-def append(collection, items):
-    if collection is None:
-        return items
-    if items is None:
-        return collection
-    if isinstance(collection, list):
-        if isinstance(items, list):
-            collection.extend(items)
-            return collection
-        else:
-            collection.append(items)
-            return collection
-    else:
-        if isinstance(items, list):
-            items.insert(0, collection)
-            return items
-        else:
-            return [ collection, items ]
-
-def get_paged_results(url):
-    results = None
-    print(url, '', end='', flush=True)
-    url = api_url + url
-    headers = {
-        'Authorization': 'token {}'.format(oauth_token)
-    }
-    params = {
-        'per_page': 100
-    }
-    while url is not None:
-        response = requests.get(url, headers=headers, params=params)
-        response.raise_for_status()
-        links = get_links(response)
-        results = append(results, response.json())
-        url = links.get('next')
-        if url is not None:
-            print('.', end='', flush=True)
-    print()
-    return results
-
-def get_cached_results(url):
-    cache_dir = os.path.expanduser(cache_path)
-    rel_url = url.removeprefix(api_url)
-    cache_filename = cache_dir + '/' + rel_url.replace('/', '_')
-    if os.path.exists(cache_filename):
-        with open(cache_filename) as json_file:
-            data = json.load(json_file)
-        return data
-    else:
-        data = get_paged_results(rel_url)
-        if not os.path.exists(cache_dir):
-            os.makedirs(cache_dir)
-        with open(cache_filename, 'w') as json_file:
-            json.dump(data, json_file, indent=3)
-        return data
-
-def get_repo_commits(owner, repo):
-    url = 'repos/{owner}/{repo}/commits'.format(owner=owner, repo=repo)
-    commits = get_cached_results(url)
-    return commits
-
-def filter_commits(commits, daterange):
-    if daterange is None:
-        return commits
-    filtered_commits = []
-    for commit in commits:
-        author = commit.get('author')
-        if author is None:
-            continue
-        if author.get('type') != 'User':
-            continue
-        date = commit.get('commit', {}).get('author', {}).get('date')
-        if in_range(date, daterange):
-            filtered_commits.append(commit)
-    return filtered_commits
-
-def get_repo_pulls(owner, repo):
-    url = 'repos/{owner}/{repo}/pulls?state=all'.format(owner=owner, repo=repo)
-    pulls = get_cached_results(url)
-    return pulls
-
-def get_org_pulls(owner, repos):
-    pulls = []
-    for repo in repos:
-        pulls.extend(get_repo_pulls(owner, repo.get('name')))
-    return pulls
-
-def filter_pulls(pulls, daterange):
-    if daterange is None:
-        return pulls
-    filtered_pulls = []
-    for pull in pulls:
-        created_at = pull.get('created_at')
-        closed_at = pull.get('closed_at')
-        if overlaps_range(created_at, closed_at, daterange):
-            filtered_pulls.append(pull)
-    return filtered_pulls
-
-def compute_median_review_duration(pulls, daterange = None):
-    durations=[]
-    for pull in pulls:
-        created_at = to_date(pull.get('created_at'))
-        closed_at = to_date(pull.get('closed_at', pull.get('merged_at')))
-        if closed_at is None and daterange is not None:
-            closed_at = daterange.get('end')
-        else:
-            closed_at = datetime.datetime.now()
-        duration = closed_at - created_at
-        durations.append(duration.total_seconds())
-        number = pull.get('number')
-        # print('   pull request {number}: created {created}, closed {closed}, duration {duration}'.format(
-        #     number=number,
-        #     created=created_at.strftime('%d/%m/%Y'),
-        #     closed=closed_at.strftime('%d/%m/%Y'),
-        #     duration=math.floor(duration.total_seconds() / (60 * 60 * 24))
-        # ))
-    median = math.floor(statistics.median(durations) / (60 * 60 * 24))
-    return median
+bots = [
+    'dependabot[bot]',
+    'dependabot-preview[bot]',
+    'buildpack-bot',
+    'github-actions[bot]',
+    'paketo-bot',
+    'concourse-bot',
+    'k8s-ci-robot',
+    'core-deps-ci-bot',
+    'codetriage-readme-bot',
+]
 
 def to_date(source):
     if source is None:
@@ -386,6 +214,8 @@ def filter_commits(commits, daterange):
             continue
         if author.get('type') != 'User':
             continue
+        if author.get('login') in bots:
+            continue
         date = commit.get('commit', {}).get('author', {}).get('date')
         if in_range(date, daterange):
             filtered_commits.append(commit)
@@ -407,6 +237,15 @@ def filter_pulls(pulls, daterange):
         return pulls
     filtered_pulls = []
     for pull in pulls:
+        author = pull.get('user')
+        if author is None:
+            continue
+        if author.get('type') != 'User':
+            continue
+        if author.get('login') in bots:
+            continue
+        if 'bot' in author.get('login'):
+            continue
         created_at = pull.get('created_at')
         closed_at = pull.get('closed_at')
         if overlaps_range(created_at, closed_at, daterange):
@@ -480,6 +319,8 @@ def get_repo_contributors(owner, repo, daterange = None):
         if author is None:
             continue
         if author.get('type') != 'User':
+            continue
+        if author.get('login') in bots:
             continue
         login = author.get('login', 'anonymous')
         contributors[login] = {
